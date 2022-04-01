bf9119cf52ec7405a372138a711dcd5b0ec5889a
akorotkov <89065267799@mail.ru>
2022-04-01 09:37:52 +0300

    Действие для пункта фильтра без галки

--- local/templates/stroyprofi/components/bitrix/catalog.smart.filter/.default/script.js
+++ local/templates/stroyprofi/components/bitrix/catalog.smart.filter/.default/script.js
@@ -220,13 +220,13 @@ JCSmartFilter.prototype.postHandler = function (result, fromCache)
 					curProp = BX.findChild(BX.findParent(this.curFilterinput, {'class':'bx-filter-parameters-box'}), {'class':'bx-filter-container-modef'}, true, false);
 					curProp.appendChild(modef);
 
-					if ($(this.curFilterinput).attr('checked') == 'checked') {
+					//if ($(this.curFilterinput).attr('checked') == 'checked') {
 						$(curProp).show();
 						var iOffsetTop = $(this.curFilterinput).offset().top - 6;
 						$(curProp).offset({top: iOffsetTop});
-					} else {
-						$(curProp).hide();
-					}
+					//} else {
+						//$(curProp).hide();
+					//}
 
 					// Преберем все параметры фильтра и скроем ненужные
 					showHideCheckBoxsInFilter();
