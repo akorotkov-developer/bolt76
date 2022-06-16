4d9d0eb81a68013aba95ba837e77d9354ffb2a4d
akorotkov <89065267799@mail.ru>
2022-06-16 22:25:56 +0300

    Поменять место логов импорта

--- local/php_interface/import/importInCron.php
+++ local/php_interface/import/importInCron.php
@@ -18,7 +18,7 @@ class importInCron
         $sOutForLog = ob_get_contents();
         ob_end_clean();
 
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/import/log_import.txt', $sOutForLog);
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/import/logs/log_import.txt', $sOutForLog);
 
         return '\\' . __METHOD__ . '();';
     }
