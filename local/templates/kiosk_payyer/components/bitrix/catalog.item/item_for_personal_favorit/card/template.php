5180c44b149915accdab1224bef5ec5424482c1f
akorotkov <89065267799@mail.ru>
2022-06-23 17:52:39 +0300

    init commit item_for_personal_favorit

--- local/templates/kiosk_payyer/components/bitrix/catalog.item/item_for_personal_favorit/card/template.php
+++ local/templates/kiosk_payyer/components/bitrix/catalog.item/item_for_personal_favorit/card/template.php
@@ -24,6 +24,10 @@ use \Bitrix\Main\Localization\Loc;
 ?>
 
 <div class="product-item">
+    <svg data-product-id="<?= $arResult['ID']?>" class="favorite-svg-icon active" title="Добавить в избранное" width="31" height="31" viewBox="0 0 24 24" fill="none" stroke="#8899a4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+    </svg>
+
 	<? if ($itemHasDetailUrl): ?>
 	<a class="product-item-image-wrapper" href="<?=$item['DETAIL_PAGE_URL']?>" title="<?=$imgTitle?>"
 			data-entity="image-wrapper">
