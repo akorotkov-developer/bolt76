1af2da905eebe28224da74c504fe102a81541048
Makar <makick200@gmail.com>
2023-07-27 15:51:31 +0300

    Добавление кнопки "Показать пароль"

--- local/templates/stroyprofi/components/bitrix/system.auth.form/popup/template.php
+++ local/templates/stroyprofi/components/bitrix/system.auth.form/popup/template.php
@@ -16,15 +16,16 @@ CJSCore::Init();
               action="<?= $arResult["AUTH_URL"] ?>">
             <? if ($arResult["BACKURL"] <> ''): ?>
                 <input type="hidden" name="backurl" value="<?= $arResult["BACKURL"] ?>"/>
-            <?endif ?>
+            <? endif ?>
             <? foreach ($arResult["POST"] as $key => $value): ?>
                 <input type="hidden" name="<?= $key ?>" value="<?= $value ?>"/>
-            <?endforeach ?>
+            <? endforeach ?>
             <input type="hidden" name="AUTH_FORM" value="Y"/>
             <input type="hidden" name="TYPE" value="AUTH"/>
             <h1>Вы не авторизованы</h1>
             <div class="form-group">
-                <input type="text" placeholder="Введите Логин" class="form-control" id="login" name="USER_LOGIN" maxlength="50" value="" size="17"/>
+                <input type="text" placeholder="Введите Логин" class="form-control" id="login" name="USER_LOGIN"
+                       maxlength="50" value="" size="17"/>
             </div>
             <script>
                 BX.ready(function () {
@@ -36,7 +37,23 @@ CJSCore::Init();
                     }
                 });
             </script>
-            <input type="password" class="form-control" placeholder="Введите Пароль" name="USER_PASSWORD" maxlength="255" size="17" autocomplete="off"/>
+            <div class="password">
+                <input type="password" id="password-input" class="form-control" placeholder="Введите Пароль" name="USER_PASSWORD" maxlength="255" size="17" autocomplete="off"/>
+                <a href="#" class="password-control"></a>
+            </div>
+            <script src="https://snipp.ru/cdn/jquery/2.1.1/jquery.min.js"></script>
+            <script>
+                $('body').on('click', '.password-control', function(){
+                    if ($('#password-input').attr('type') == 'password'){
+                        $(this).addClass('view');
+                        $('#password-input').attr('type', 'text');
+                    } else {
+                        $(this).removeClass('view');
+                        $('#password-input').attr('type', 'password');
+                    }
+                    return false;
+                });
+            </script>
             <? if ($arResult["SECURE_AUTH"]): ?>
                 <span id="bx_auth_secure<?= $arResult["RND"] ?>"
                       title="<? echo GetMessage("AUTH_SECURE_NOTE") ?>" style="display:none">
@@ -49,13 +66,17 @@ CJSCore::Init();
                 <script type="text/javascript">
                     document.getElementById('bx_auth_secure<?=$arResult["RND"]?>').style.display = 'inline-block';
                 </script>
-            <?endif ?>
+            <? endif ?>
             <div class="pass_forgot">
-                <a class="pass_forgot" href="/account/forgot/?forgot_password=yes&backurl=%2Faccount%2Fauth%2F">Забыли пароль?</a>
+                <a class="pass_forgot" href="/account/forgot/?forgot_password=yes&backurl=%2Faccount%2Fauth%2F">Забыли
+                    пароль?</a>
             </div>
-            <input type="submit" class="btn btn-primary btn-popup" name="Login" value="<?= GetMessage("AUTH_LOGIN_BUTTON") ?>"/>
-            <input type="button" onclick="window.location.href='/account/register/index.php';" class="btn btn-popup" value="Зарегистрироваться"/>
-            <input type="button" class="btn btn-popup cancel continue_without_registration" value="Продолжить без регистрации">
+            <input type="submit" class="btn btn-primary btn-popup" name="Login"
+                   value="<?= GetMessage("AUTH_LOGIN_BUTTON") ?>"/>
+            <input type="button" onclick="window.location.href='/account/register/index.php';" class="btn btn-popup"
+                   value="Зарегистрироваться"/>
+            <input type="button" class="btn btn-popup cancel continue_without_registration"
+                   value="Продолжить без регистрации">
         </form>
     <? endif ?>
 </div>
