1f809bf89ec07e987c35271cc48900e9f05b1922
GizSH <makick200@gmail.com>
2023-08-07 09:09:35 +0300

    Обновил до актуальных страницы контактов и панель с What's Up, временно скрыл новые стили для кнопок листания страниц

--- includes/contact_email.php
+++ includes/contact_email.php
@@ -1,10 +1,2 @@
-<?php
-if ($arParams['IS_KIOSK_PAYER']) {?>
-    <span style="font-family: Verdana; font-size: 14pt; color: #f7941d;">
-        <span style="color: #f7941d; font-size: 12pt;">mail@strprofi.ru</span>
-    </span>
-<?php } else {?>
-    <span style="font-family: Verdana; font-size: 14pt; color: #f7941d;">
-        <a href="mailto:mail@strprofi.ru"><span style="color: #f7941d; font-size: 12pt;">mail@strprofi.ru</span></a>
-    </span>
-<?php }?>
+<a href="mailto:mail@strprofi.ru"><span style="color: #f7941d; font-size: 12pt;">mail@strprofi.ru</span></a> <br>
+ <span style="color: #f7941d; font-size: 12pt;">WhatsApp: +7(906)-639-51-52</span><br>
