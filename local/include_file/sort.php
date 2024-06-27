9d92ece26de65520c2c99d0702b8411259127a30
akorotkov <89065267799@mail.ru>
2024-06-27 20:38:34 +0300

    Сортировка в товарах

---
+++ local/include_file/sort.php
@@ -0,0 +1,32 @@
+<?php
+use Bitrix\Main\Application;
+
+$request = Application::getInstance()->getContext()->getRequest();
+$sortGetParam = $request->get('catalog_sort');
+
+$sortValues = [
+    'default' => 'По умолчанию',
+    'price_asc' => 'Подешевле',
+    'price_desc' => 'Подороже',
+    'name' => 'По названию'
+];
+?>
+<div class="b-sort-top">
+    <span class="sort-ass"> Сортировать:</span>
+
+    <select id="sort-select">
+        <?php foreach ($sortValues as $code => $value) {?>
+            <option value="<?= $code?>" <?= ($code == $sortGetParam) ? 'selected' : ''?>><?= $value?></option>
+        <?php } ?>
+    </select>
+
+    <script>
+        $('#sort-select').change(function() {
+            console.log($(this).val(), '$(this).val()!!!');
+            console.log($(this).val(), '$(this).val()');
+
+            var baseUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
+            window.location.href = baseUrl + '?tst=tst&clear_cache=Y&catalog_sort=' + $(this).val();
+        });
+    </script>
+</div>
