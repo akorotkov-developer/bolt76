63b47a6129a70cb1b82478076a1b997fd08f9740
admin <89065267799@mail.ru>
2024-08-30 08:56:57 +0300

    Скрипт для установки кнопок удалить в списке товаров

--- local/templates/stroyprofi/components/bitrix/catalog/cat/profi/catalog.section/section_changes/component_epilog.php
+++ local/templates/stroyprofi/components/bitrix/catalog/cat/profi/catalog.section/section_changes/component_epilog.php
@@ -47,3 +47,58 @@ foreach($arFavorites as $k => $favoriteItem):
         });
     </script>
 <?php endforeach; ?>
+
+<script>
+    $( document ).ready(function() {
+        window.basketController = {
+            setLinks: function () {
+                $.ajax({
+                    url: '/local/ajax/setbasketlinks.php',
+                    method: 'get',
+                    data: {},
+                    success: function (data) {
+                        var products = JSON.parse(data);
+                        var elementId;
+
+                        $('.product-layout').each(function(i, obj) {
+                            elementId = $(obj).attr('data-elementid');
+
+                            if (products[elementId]) {
+                                $(obj).find('.is-in-basket').html('<div class="in_basket">' + Number(products[elementId].QUANTITY) + ' в корзине <img class="in_basket_delete" data-element="' + elementId + '" src="/local/templates/stroyprofi/components/bitrix/catalog/cat/profi/catalog.section/section_descr/images/trash.png"></div>');
+                            }
+                        });
+                        $('.element_product_tr').each(function(i, obj) {
+                            elementId = $(obj).attr('data-elementid');
+
+                            if (products[elementId]) {
+                                $(obj).find('.is-in-basket').html('<div class="in_basket">' + Number(products[elementId].QUANTITY) + ' в корзине <img class="in_basket_delete" data-element="' + elementId + '" src="/local/templates/stroyprofi/components/bitrix/catalog/cat/profi/catalog.section/section_descr/images/trash.png"></div>');
+                            }
+                        });
+                    }
+                });
+            }
+        };
+        window.basketController.setLinks();
+
+        // Удаление товара из корзины
+        $('body').on('click', '.in_basket_delete', function(e) {
+            var param = 'idBasketElement=' + $(this).attr('data-element');
+            var inBasketBlock = $(this).parent();
+            $.ajax({
+                url:     '/local/ajax/deletebasketelement.php', // URL отправки запроса
+                type:     'GET',
+                dataType: 'html',
+                data: param,
+                success: function(response) {
+                    if ($.trim(response) != '') {
+                        $('.cart_info_holder').html(response);
+                    }
+                    inBasketBlock.remove();
+                },
+                error: function(jqXHR, textStatus, errorThrown){ // Ошибка
+                    console.log('Error: '+ errorThrown);
+                }
+            });
+        });
+    });
+</script>
